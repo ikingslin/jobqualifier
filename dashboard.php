a5cb54527b059d4b30acc851e360bef9d94f33ba
ikingslin <96917142+ikingslin@users.noreply.github.com>
2022-02-07 09:15:12 +0530

    adminhome

--- dashboard.php
+++ dashboard.php
@@ -1,48 +1,45 @@
 <?php
 session_start();
+session_abort();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="assets/index.css">
-</head>
-<body>
-    <header class="d-flex flex-wrap justify-content-left py-2 border-bottom bg-light">
-        <div class="d-flex align-items-center  me-md-auto">
-            <img src="assets/images/logo.png" alt="No Image" id="logo" style="margin-left: 2%;">
-            <h4 class="fs-4" id="companytitle" style="margin-left: 2%;">JobQualifier</h4>
-            <span>AdminConsole</span>
-        </div>
-        
-        <ul class="nav nav-pills align-items-center" style="margin-right:2%">
-            <li class="nav-item" style="margin-right:15px;">
-                <?php echo $_SESSION['login_user'];?>
-            </li>
-            <li class="nav-item">
-                <a href="index.php">Logout</a>
-            </li>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="assets/index.css">
+        <link rel="stylesheet" href="assets/sidebar.css">
+    </head>
+    <body>
+        <header class="d-flex flex-wrap justify-content-left py-2 border-bottom bg-light">
+            <div class="d-flex align-items-center  me-md-auto">
+                <img src="assets/images/logo.webp" alt="No Image" id="logo" style="margin-left: 2%;" class="rounded">
+                <h5 class="fs-2 companytitle" style="margin-left: 2%;">JobQualifier</h5>
+            </div>
             
-        </ul>
-    </header><br>
-    <div class="d-flex" style = "height:85vh;max-height:100%;min-height:fitcontent">
-        <div class="container-fluid  border-end" style ="max-width:fit-content">
-            <ul class="list-group" style = "text-decoration:none;">
-                <li class="list-group-item"><a href="home.html" target="maincontent">Home</a></li>
-                <li class="list-group-item"><a href="#" target="maincontent">Drive Settings</a></li>
-                <li class="list-group-item"><a href="#" target="maincontent">Candidate Filtering</a></li>
-                <li class="list-group-item"><a href="index.php">Logout</a></li>
+            <ul class="nav nav-pills align-items-center" style="margin-right:2%">
+                <li class="nav-item" style="margin-right:15px;">
+                    <?php echo $_SESSION['login_name'];?>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php">
+                        <input type="button" value="Logout"  class="btn btn-primary">
+                    </a>
+                </li>
             </ul>
+        </header>
+        <div class="sidebar">
+            <a class="active" href="home.php">Home</a>
+            <a href="">Drive Settings</a>
+            <a href="">Question Update</a>
+            <a href="#list">Candidate Grading</a>
+            <a href="#filter">Candidate Filtering</a>
+            <a href="index.php">Logout</a>
         </div>
-        <div class="container-fluid embed-responsive">
-        <iframe src="home.html" name="maincontent" scrolling="no" class="embed-responsive-item col-sm-12" style="height:85vh;"></iframe>
-
-        </div>
-    </div>
+           
 </body>
 </html>
---
+++ home.php
@@ -0,0 +1,96 @@
+<?php
+  if(!isset($_SESSION)) 
+  { 
+    session_start(); 
+  }
+  $conn = mysqli_connect("localhost","root","","jobqualifier");
+  if(!$conn)
+  {
+    die("Connection to DB failed with : ".mysqli_connect_error());
+  }
+  $roles = 'SELECT `Name`,COUNT(roles.roleid) as rcount FROM roles RIGHT join application on roles.roleid = application.roleid group by roles.roleid;';
+  $result = mysqli_query($conn,$roles);
+  $rol = array();
+  $count = array();
+  $i=0;
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $rol[$i] = $row["Name"];
+      $count[$i] = $row["rcount"];
+      $i++;
+    }
+  } else {
+    echo "";
+  }
+  
+?>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="assets/index.css">
+        <link rel="stylesheet" href="assets/sidebar.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    </head>
+    <body>
+    <header class="d-flex flex-wrap justify-content-left py-2 border-bottom bg-light">
+            <div class="d-flex align-items-center  me-md-auto">
+                <img src="assets/images/logo.webp" alt="No Image" id="logo" style="margin-left: 2%;" class="rounded">
+                <h5 class="fs-2 companytitle" style="margin-left: 2%;">JobQualifier</h5>
+            </div>
+            
+            <ul class="nav nav-pills align-items-center" style="margin-right:2%">
+                <li class="nav-item" style="margin-right:15px;">
+                    <?php echo $_SESSION['login_name'];?>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php">
+                        <input type="button" value="Logout"  class="btn btn-primary">
+                    </a>
+                </li>
+            </ul>
+        </header>
+        <div class="sidebar">
+            <a class="active" href="home.php">Home</a>
+            <a href="">Drive Settings</a>
+            <a href="">Question Update</a>
+            <a href="#list">Candidate Grading</a>
+            <a href="#filter">Candidate Filtering</a>
+            <a href="index.php">Logout</a>
+        </div>
+        
+    <div class="content">
+            <div class="container">
+              <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+            </div>
+    </div>
+    </body>
+    <script>
+        var xValues = <?php echo json_encode($rol); ?>;
+        var yValues = <?php echo json_encode($count); ?>;
+        var barColors = ["red", "green","blue","orange","brown"];
+        
+        new Chart("myChart", {
+          type: "bar",
+          data: {
+            labels: xValues,
+            datasets: [{
+              backgroundColor: barColors,
+              data: yValues
+            }]
+          },
+          options: {
+            legend: {display: false},
+            title: {
+              display: true,
+              text: "Applicants"
+            }
+          }
+        });
+        </script>
+    
+</html>
