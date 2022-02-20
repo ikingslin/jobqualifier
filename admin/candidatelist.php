8050a7c899cbf5042ece7a39c32cd6cd6712222e
ikingslin <96917142+ikingslin@users.noreply.github.com>
2022-02-20 11:13:08 +0530

    titles

--- admin/candidatelist.php
+++ admin/candidatelist.php
@@ -28,7 +28,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title>Adding Roles</title>
+        <title>Candidate List</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
