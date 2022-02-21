74bedbcee5b9307faccc0222b43bf426810b0af1
ikingslin <96917142+ikingslin@users.noreply.github.com>
2022-02-21 19:12:48 +0530

    hires

---
+++ admin/selected.php
@@ -0,0 +1,31 @@
+<?php
+    include('../aauth.php');
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }
+    $conn = mysqli_connect("localhost","root","","jobqualifier");
+    if(!$conn)
+    {
+        die("Connection to DB failed with : ".mysqli_connect_error());
+    }
+    $cid = $_POST['canset'];
+    //echo $_POST['canset'];
+    $i=0;
+    $email = $_SESSION['login_user'];
+    //echo $email;
+    $sql = "SELECT * FROM `admin` WHERE `email`='$email'";
+    mysqli_query($conn,$sql);
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_assoc($result);
+    $admin = $row['AdminID'];
+    while(isset($cid[$i]))
+    {
+        $var = explode('/',$cid[$i]);
+        //echo $var[0]." ".$var[1];
+        $sql = "INSERT INTO `hires`(`AdminID`, `cid`, `application_id`, `status`) VALUES('$admin','$var[0]','$var[1]','clearedfilter')";
+        mysqli_query($conn,$sql);
+         $i++;
+    }
+    mysqli_close($conn);
+?>
